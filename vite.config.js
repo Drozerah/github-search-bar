923384a1b8de3f4a51f498beff11f7eca41913b2
Drozerah <drozerah@gmail.com>
2023-03-07 10:04:12 +0100

    chore(./vite.config.js): add @utils alias

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       'node-fetch': 'isomorphic-fetch', // fix: https://stackoverflow.com/a/73095593/9370788
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url))
     }
   }
