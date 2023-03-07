11acb13d95443985a9a2bb7698d864f1a655d96f
Drozerah <drozerah@gmail.com>
2023-03-07 10:05:48 +0100

    feat(src/utils/index.js): add reverseArray filter function

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * Reverses an array without modifying the original array.
+ *
+ * @param {Array} array - The input array to be reversed.
+ * @returns {Array} - A new array with the elements in reversed order.
+ */
+export function reverseArray (array) {
+  return array.slice().reverse()
+}
