4fbecdadcdca011ca9026070e961a3a46e565d92
Drozerah <drozerah@gmail.com>
2023-03-07 15:16:30 +0100

    style(src/components/basecards.vue): format code

--- src/components/BaseCards.vue
+++ src/components/BaseCards.vue
@@ -7,18 +7,17 @@
         >
         <div class="card-content">
           <div class="content">
-          <div class="content-header">
-            <div
-              v-if="item.searchTerm"
-            >Term of search : <span class="tag is-link is-light">{{ item.searchTerm }}</span>
+            <div class="content-header">
+              <div
+                v-if="item.searchTerm"
+              >Term of search : <span class="tag is-link is-light">{{ item.searchTerm }}</span>
+              </div>
+              <button
+                class="button is-outlined is-small"
+                v-if="isDeleteBtn"
+                @click="store.removeItemById(item.id)"
+              >Delete</button>
             </div>
-            <button
-              class="button is-outlined is-small"
-              v-if="isDeleteBtn"
-              @click="store.removeItemById(item.id)"
-            >Delete</button>
-          </div>
-            
             <h3 class="content-title">{{ item.name }}</h3>
             <p>{{ item.description }}</p>
             <a :href="item.html_url" target="_blank" rel="noopener noreferrer">{{ item.html_url }}</a>
@@ -29,9 +28,9 @@
 </template>
 
 <script setup>
+import { reverseArray } from '@utils'
 import { useSelectionStore } from '@stores/SelectionStore'
 const store = useSelectionStore()
-import { reverseArray } from '@utils'
 defineProps({
   data: {
     type: Array
