5b0e6f0dba438dceee84980a8c88c418cda56595
Drozerah <drozerah@gmail.com>
2023-05-25 10:30:41 +0200

    refactor(src/stores/bootstore.js): update password

--- src/stores/BootStore.js
+++ src/stores/BootStore.js
@@ -2,6 +2,6 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useBootStore = defineStore('bootStore', () => {
-  const password = ref('izypay')
+  const password = ref('peaks')
   return { password }
 })
